24cddf33a226f0028f0d05586d91f3ef2b8719a3
Lee-JinHyeok <j5539538@daum.net>
2021-12-25 00:03:36 +0900

    STEP 4.COMMIT 2021-12-25
    - FeedRepository 추상클래스 삭제. FeedRepositoryJpa로 대체FeedRepository. @PersistenceContext 를 이용한 EntityManagerFactory, EntityManger 컨트롤
    - HomeController 삭제. index.html 을 기본 template
    - yml 설정 수정. sql 및 parameter logging 출력
    - SpringConfig 삭제. @을 이용한 컴포넌트 컨트롤

--- src/main/java/daleespring/feed/FeedRepository.java
+++ src/main/java/daleespring/feed/FeedRepository.java
@@ -1,10 +1,29 @@
 package daleespring.feed;
 
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedRepository {
-    Feed saveFeed(Feed feed);
-    Optional<Feed> findByIdFeed(Long feedId);
-    List<Feed> findByAllFeed();
+@Repository
+public class FeedRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public Feed saveFeed(Feed feed) {
+        entityManager.persist(feed);
+        return feed;
+    }
+
+    public Optional<Feed> findByIdFeed(Long feedId) {
+        return Optional.ofNullable(entityManager.find(Feed.class, feedId));
+    }
+
+    public List<Feed> findByAllFeed() {
+        return entityManager.createQuery("select f from Feed f", Feed.class).getResultList();
+    }
 }
