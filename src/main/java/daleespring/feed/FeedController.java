402ee79fab1f8e348e6148a1c1a4cf19086bc6ab
Lee-JinHyeok <j5539538@daum.net>
2021-12-30 23:49:18 +0900

    STEP 7. COMMIT 2021-12-30
    - Feed 엔티티 @JsonFormat(pattern ="")을 사용해 날짜 형식 포맷, @CreateTimestamp, @UpdateTimestamp 을 활용한 수정일 및 작성일 자동 기입
    - FeedMoodColor -> MoodColor 수정
    - FeedController. List 메서드 수정, add 메서드 데이터 저장, html에서 넘어온 값 @Vaild @NotEmpty 유효성 검사 후 feed_add.html 에서 유효성 검사 걸린 값 Message 출력 처리

--- src/main/java/daleespring/feed/FeedController.java
+++ src/main/java/daleespring/feed/FeedController.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -17,9 +20,10 @@ public class FeedController {
     private final FeedService feedService;
 
     @GetMapping("list")
-    public String feedList(){
+    public String feedList(Model model){
         log.info(" === " + getClass() + " === " + "예시 log");
 
+        model.addAttribute("feedList", feedService.findByAllFeed());
         return "feed/feed_list";
     }
 
@@ -27,14 +31,23 @@ public class FeedController {
     public String feedAdd(Model model){
 
         model.addAttribute("feedAddForm", new FeedAddForm());
-        model.addAttribute("feedMoodColor", FeedMoodColor.values());
         return "feed/feed_add";
     }
 
     @PostMapping(value = "add")
-    public String feedAdd(FeedAddForm feedForm){
+    public String feedAdd(@Valid FeedAddForm feedForm, BindingResult feedFromBindingResult){
 
+        if (feedFromBindingResult.hasErrors()) {
+            return "feed/feed_add";
+        }
 
+        Feed feed = new Feed();
+        feed.setTitle(feedForm.getTitle());
+        feed.setContent(feedForm.getContent());
+        feed.setReservationDate(feedForm.getReservationDate());
+        feed.setMoodColor(feedForm.getMoodColor());
+
+        feedService.saveFeed(feed);
         return "redirect:/feed/list";
     }
 
