0b0ca16bbfa74a8df0dcf78d01e570db2233b61f
jhlee <j5539538@daum.net>
2022-01-14 16:57:32 +0900

    STEP 13. COMMIT 2022-01-14
    - member_join.html 닉네임 위치 수정
    - member_login.html 페이지 생성
    - MemberController member_login.html 맵핑

--- src/main/java/daleespring/member/MemberLoginForm.java
+++ src/main/java/daleespring/member/MemberLoginForm.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter @Setter
-public class MemberJoinForm {
+public class MemberLoginForm {
 
     @NotBlank(message = "이메일을 입력해주세요")
     @Email(message = "올바른 이메일 주소를 입력해주세요")
@@ -17,13 +17,4 @@ public class MemberJoinForm {
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요")
     private String password;
-
-    @NotBlank(message = "비밀번호 확인을 입력해주세요")
-    @Size(min = 8, max = 20, message = "비밀번호 확인은 8자 이상 20자 이하로 입력해주세요")
-    private String passwordCheck;
-
-    @NotBlank(message = "닉네임을 입력해주세요")
-    @Size(min = 2, max = 20,message = "닉네임은 2자 이상 20자 이하로 입력해주세요")
-    private String nickname;
-
 }
