845e6100bc816d4acf6223be63bab987770f22ac
jhlee <j5539538@daum.net>
2022-01-14 10:46:12 +0900

    STEP 13. COMMIT 2022-01-14
    - Feed Entity 수정으로 인한 feed_list.html 수정
    - FeedAddForm @NotNull -> @NotBlank 로 수정 공백처리위함, @Size 추가

--- src/main/java/daleespring/feed/FeedModifyForm.java
+++ src/main/java/daleespring/feed/FeedModifyForm.java
@@ -5,21 +5,25 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter @Setter
 public class FeedModifyForm {
 
-    @NotEmpty(message = "제목을 입력해주세요!")
+    @NotBlank(message = "제목을 입력해주세요")
+    @Size(max = 200, message = "제목은 최대 200자입니다")
     private String title;
 
     @Lob
-    @NotEmpty(message = "내용을 입력해주세요!")
+    @NotBlank(message = "내용을 입력해주세요")
+    @Size(max = 30000, message = "내용은 최대 30000자입니다")
     private String content;
 
-    @NotNull(message = "상태를 선택해주세요!")
+    @NotNull(message = "상태를 선택해주세요")
     private MoodColor moodColor;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
